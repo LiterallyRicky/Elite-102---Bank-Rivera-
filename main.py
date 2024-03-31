4b54fb0aeff6da1cc41d0cbaa2b7b0c4cd04e75c
RickyRiv <ricky.rivera0429@gmail.com>
2024-03-30 23:46:12 -0500

    Added a new welcome message and menu

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("hello")
-print("new change")
+def welcome_message():
+    print("Welcome to our application!")
+
+def main_menu():
+    print("Main Menu:")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+
+if __name__ == "__main__":
+    welcome_message()
+    main_menu()
